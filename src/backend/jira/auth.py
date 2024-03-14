890f61566f79c15046b2d8e270c065c4f25b7901
ch3ll <ch3ll@localhost.localdomain>
2024-03-14 13:21:49 -0600

    Fix various issues

--- src/backend/jira/auth.py
+++ src/backend/jira/auth.py
@@ -3,7 +3,15 @@ from jira import JIRA
 
 class Auth():
     def __init__(self, opts):
+        default_auth = "token_auth"
+        validate_auths = {"basic": "basic_auth", "token": default_auth}
         self.server = opts["jira"]["server"]
         self.user = opts["jira"]["auth"]["user"]
         self.passwd = opts["jira"]["auth"]["passwd"]
-        self.jira = JIRA(server=self.server, basic_auth=(self.user, self.passwd))
+        self.auth_type = opts["jira"]["auth"].get("auth_type")
+        auth_type = validate_auths.get(self.auth_type, default_auth)
+        if auth_type == default_auth:
+            kwargs = {auth_type: (self.passwd)}
+        elif auth_type == "basic_auth":
+            kwargs = {auth_type: (self.user, self.passwd)}
+        self.jira = JIRA(server=self.server, **kwargs)
--- src/backend/jira/issues.py
+++ src/backend/jira/issues.py
@@ -1,4 +1,6 @@
 import backend.jira.auth
+import time
+import utils.time_estimate
 
 
 class Issues(backend.jira.auth.Auth):
@@ -6,6 +8,22 @@ class Issues(backend.jira.auth.Auth):
         super().__init__(opts)
         self.opts = opts
         self.fields = self.opts.get("fields")
+
+    def parse_comments(self, comments):
+        """
+        Function to parse all comments. If comment_filter is
+        set only return that comment. If comment_filter is not
+        set return latest comment
+        """
+        for comment in comments:
+            body = comment["body"]
+            comment_filter = self.opts["jira"]["comment_filter"]
+            if comment_filter:
+                if body.startswith(comment_filter):
+                    body = body.replace(comment_filter, "")
+                    return body
+        return body
+
 
     def search_issues(self):
         issues = {"issues": {}}
@@ -16,10 +34,27 @@ class Issues(backend.jira.auth.Auth):
             issue = self.jira.issue(get_issue.key)
             issue_key = get_issue.key
             issues["issues"][issue_key] = {}
+            issues["issues"][issue_key]["key"] = issue_key
             issues["issues"][issue_key]["title"] = issue.fields.summary
-            issues["issues"][issue_key]["estimate"] = issue.fields.timeestimate
-            comment = issue.raw['fields']['comment']["comments"]
+            issues["issues"][issue_key]["status"] = issue.fields.status.name
+            estimate = None
+            time_estimate = issue.fields.timeestimate
+            if time_estimate:
+                estimate = utils.time_estimate.convert_estimate(time_estimate)
+
+            issues["issues"][issue_key]["estimate"] = estimate
+
             issues["issues"][issue_key]["url"] =  issue.raw['self']
-            if comment:
-                issues["issues"][issue_key]["comment"] = comment[0]['body']
+
+            # comments
+            comments = issue.raw['fields']['comment']["comments"]
+            if comments:
+                issues["issues"][issue_key]["comment"] = self.parse_comments(comments)
+
+            # custom fields
+            custom_fields = self.opts["jira"].get("custom_fields")
+            if custom_fields:
+                for field in custom_fields:
+                    issues["issues"][issue_key][field] = issue.raw["fields"][field]
+
         return issues
